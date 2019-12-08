72dd8fceb711d006c559b90187573a690b5a3bf0
Harry Brown <harrybrown98@gmail.com>
2019-12-08 10:45:54 -0800

    setup

--- setup.py
+++ setup.py
@@ -3,12 +3,27 @@ from setuptools import setup
 with open('README.md', 'r') as readme:
     long = readme.read()
 
+version = '0.0.1'
+name = 'dispatch'
+url = f'https://github.com/harrybrwn/{name}'
+
 setup(
-   name='dispatch',
-   version='0.0.1',
-   description='A low information-redundancy cli framework.',
-   long_description=long,
+   name=name,
+   version=version,
    author='Harrison Brown',
    author_email='harrybrown98@gmail.com',
-   packages=['dispatch'],
+   license='Apache 2.0',
+   packages=[name],
+   description='A low information-redundancy cli framework.',
+   long_description=long,
+   long_description_content_type="text/markdown",
+   url=url,
+   download_url=f'{url}/archive/v{version}.tar.gz',
+   keywords=['command line', 'cli', 'framework', 'tool', 'simple'],
+   install_requires=['jinja2'],
+   classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License 2.0",
+        "Operating System :: OS Independent",
+    ],
 )
